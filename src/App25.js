d8897e479778fc1d5e9aac3881826d3205d6181a
adityarevanth <adityarevanth@lyrostech.com>
2024-03-18 17:12:44 +0530

    App29

--- src/App25.js
+++ src/App25.js
@@ -1,12 +1,14 @@
-export default function App25(){
-    const divRef = useRef(null);
-    const handleColorChange=()=>{
-        divRef.current.style.backgroundColor='silver';
-    };  
-    return(
-        <div ref={divRef}>
-            <p>Hello This is Aditya</p>
-            <button onClick={handleColorChange}>Click</button>
-        </div>
-    );
+import React, { useRef } from 'react';
+
+export default function App25() {
+  const divRef = useRef(null);
+  const handleColorChange = () => {
+    divRef.current.style.backgroundColor = 'silver';
+      };
+  return (
+    <div ref={divRef}>
+      <p>Hello! Welcom to Mars</p>
+      <button onClick={handleColorChange}>Click</button>
+    </div>
+  );
 };
